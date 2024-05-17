81f5548fc9dd3ec952460a54ceea3c680f37d8a9
ttrifle <Bigtoolforyou!@>
2024-05-17 22:52:18 +0300

    stage 3 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -19,8 +19,23 @@ fun main() {
         dictionary.add(Word(listOfLine[0], listOfLine[1], listOfLine[2].toInt() ?: 0))
     }
 
-    dictionary.forEach {
-        println(it)
+    while (true) {
+
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+
+        when (readln().toInt()) {
+            1 -> println("В разработке...")
+            2 -> {
+                val learnedWord = dictionary.filter { it.correctAnswersCount >= 3 }.size
+                val numberOfWords = dictionary.size
+                val percentOfLearnedWords = (learnedWord.toDouble() / numberOfWords.toDouble() * 100).toInt()
+                println("Выучено $learnedWord из $numberOfWords слов | $percentOfLearnedWords%")
+            }
+
+            0 -> break
+            else -> println("Не корректный выбор меню")
+        }
+
     }
 
 }

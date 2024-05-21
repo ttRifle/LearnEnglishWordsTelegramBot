844806b7dd344deae5ec870c702c659335599612
ttrifle <Bigtoolforyou!@>
2024-05-22 02:12:04 +0300

    stage 4 fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,13 +26,16 @@ fun main() {
 
         when (readln().toInt()) {
             1 -> {
-                while (dictionary.filter { it.correctAnswersCount < 3 }.isNotEmpty()) {
-                    val notLearnedWords = dictionary.filter { it.correctAnswersCount < 3 }
-                    val answerWords = notLearnedWords.shuffled().take(4)
+
+                val notLearnedWords = dictionary.filter { it.correctAnswersCount < CORRECT_ANSWERS_TO_LEARN }
+
+                while (notLearnedWords.isNotEmpty()) {
+
+                    val answerWords = notLearnedWords.shuffled().take(NUMBER_OF_ANSWER_OPTIONS)
                     val wordToLearn = answerWords.random()
 
-                    println(wordToLearn)
-                    println(answerWords)
+                    println(wordToLearn.original)
+                    println(answerWords.joinToString { (answerWords.indexOf(it) + 1).toString() + " - " + it.translate })
 
                     when (readln().toInt()) {
                         0 -> break
@@ -41,15 +44,18 @@ fun main() {
 
                 }
 
-                if (dictionary.filter { it.correctAnswersCount < 3 }.isEmpty()) println("Вы выучили все слова")
+                if (notLearnedWords.isEmpty()) println("Вы выучили все слова")
 
             }
 
             2 -> {
+
                 val learnedWord = dictionary.filter { it.correctAnswersCount >= 3 }.size
                 val numberOfWords = dictionary.size
                 val percentOfLearnedWords = (learnedWord.toDouble() / numberOfWords.toDouble() * 100).toInt()
+
                 println("Выучено $learnedWord из $numberOfWords слов | $percentOfLearnedWords%")
+
             }
 
             0 -> break
@@ -59,3 +65,6 @@ fun main() {
     }
 
 }
+
+const val CORRECT_ANSWERS_TO_LEARN = 3
+const val NUMBER_OF_ANSWER_OPTIONS = 4
